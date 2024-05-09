4633a79e14cb6dbffd3e8957755aa83fa8e3dff6
Lê Ngọc Hiếu <142372393+hieule245@users.noreply.github.com>
2024-05-09 07:44:17 +0700

    express

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express'),
+    http = require('http');
+
+const hostname = 'localhost';
+const port = 2448;
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
